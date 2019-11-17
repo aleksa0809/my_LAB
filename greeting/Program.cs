262628915640a754b62963bc4524408ba4bf496a
aleksandra sagitova <aleksandrasagitova@aleksandras-MacBook-Pro.local>
2019-11-17 19:45:04 +0100

    greeting

---
+++ greeting/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your first name ");
+            string first_name = Console.ReadLine();
+            Console.WriteLine("enter your second  name ");
+            string second_name = Console.ReadLine();
+
+            Console.WriteLine(" Hello " + first_name + " " + second_name + ".");
+        }
+    }
+}
